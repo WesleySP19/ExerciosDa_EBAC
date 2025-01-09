2cba76d48f5dc50898f6d9b01f88c136e3b2b88b
Wesley <wesleyDev42@outlook.com>
2025-01-09 15:24:35 -0300

    Update LeituraValorNumerico.java
    arrumando o erro

--- Modulo_09/atividade  de boce/LeituraValorNumerico.java
+++ Modulo_09/atividade  de boce/LeituraValorNumerico.java
@@ -4,15 +4,19 @@ public class LeituraValorNumerico {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        // Lendo um número inteiro
         System.out.print("Digite um número inteiro: ");
-        int numeroInteiro = scanner.nextInt();
+        int numeroInteiroPrimitivo = scanner.nextInt();
+        // Convertendo o valor primitivo para o tipo wrapper Integer
+        Integer numeroInteiro = Integer.valueOf(numeroInteiroPrimitivo);
 
-
+        // Lendo um número de ponto flutuante (double)
         System.out.print("Digite um número de ponto flutuante: ");
-        double numeroDecimal = scanner.nextDouble();
+        double numeroDecimalPrimitivo = scanner.nextDouble();
+        // Convertendo o valor primitivo para o tipo wrapper Double
+        Double numeroDecimal = Double.valueOf(numeroDecimalPrimitivo);
 
-
+        // Mostrando os valores lidos (com as variáveis do tipo wrapper)
         System.out.println("Número inteiro: " + numeroInteiro);
         System.out.println("Número decimal: " + numeroDecimal);
 
